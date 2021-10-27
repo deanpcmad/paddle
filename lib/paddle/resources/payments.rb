aface1c5fe9508f966027c1716b27335473c6288
Dean Perry <dean@deanpcmad.com>
2021-10-27 15:10:01 +0100

    should be body

--- lib/paddle/resources/payments.rb
+++ lib/paddle/resources/payments.rb
@@ -2,19 +2,19 @@ module Paddle
   class PaymentsResource < Resource
 
     def list(**params)
-      response = post_request("2.0/subscription/payments", params)
+      response = post_request("2.0/subscription/payments", body: params)
       Collection.from_response(response, type: Payment)
     end
 
     def reschedule(payment_id:, date:)
       attrs = {payment_id: payment_id, date: date}
-      response = post_request("2.0/subscription/payments_reschedule", attrs)
+      response = post_request("2.0/subscription/payments_reschedule", body: attrs)
       return true if response.success?
     end
 
     def refund(order_id:, **params)
       attrs = {order_id: order_id}
-      response = post_request("2.0/payment/refund", attrs.merge(params))
+      response = post_request("2.0/payment/refund", body: attrs.merge(params))
       PaymentRefund.new(response.body["response"])
     end
 

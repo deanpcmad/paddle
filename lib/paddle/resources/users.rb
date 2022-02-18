8e9f56959a1ab1d959303fdb21d1ef34efacc807
Dean Perry <dean@deanpcmad.com>
2022-02-18 08:39:11 +0000

    pause and unpause subscriptions

--- lib/paddle/resources/users.rb
+++ lib/paddle/resources/users.rb
@@ -12,6 +12,18 @@ module Paddle
       User.new(response.body["response"]) if response.success?
     end
 
+    def pause(subscription_id:, **params)
+      attrs = {subscription_id: subscription_id, pause: true}
+      response = post_request("2.0/subscription/users/update", body: attrs.merge(params))
+      User.new(response.body["response"]) if response.success?
+    end
+
+    def unpause(subscription_id:, **params)
+      attrs = {subscription_id: subscription_id, pause: false}
+      response = post_request("2.0/subscription/users/update", body: attrs.merge(params))
+      User.new(response.body["response"]) if response.success?
+    end
+
     def update_postcode(subscription_id:, postcode:)
       attrs = {subscription_id: subscription_id, postcode: postcode}
       response = post_request("2.0/subscription/users/postcode", body: attrs)

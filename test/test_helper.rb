cf86dddf46085e4c260ef17a6a1f1dd075c06759
Dean Perry <dean@deanpcmad.com>
2022-02-22 18:25:38 +0000

    bundle exec

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,19 @@
-# frozen_string_literal: true
-
-$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "paddle"
+require "minitest/autorun"
 require "faraday"
 require "json"
 
-require "minitest/autorun"
+class Minitest::Test
+  def stub_response(fixture:, status: 200, headers: {"Content-Type" => "application/json"})
+    [status, headers, File.read("test/fixtures/#{fixture}.json")]
+  end
+
+  def stub_request(path, response:, method: :get, body: {})
+    Faraday::Adapter::Test::Stubs.new do |stub|
+      arguments = [method, "/v2/#{path}"]
+      arguments << body.to_json if [:post, :put, :patch].include?(method)
+      stub.send(*arguments) { |env| response }
+    end
+  end
+end

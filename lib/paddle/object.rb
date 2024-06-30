72a516a845b3b6b1e22838356760c9e4d6a462d6
Tejas <tejas-shetty@outlook.com>
2024-06-30 20:10:13 +0000

    Paddle::Object#update ⚙️

--- lib/paddle/object.rb
+++ lib/paddle/object.rb
@@ -15,5 +15,26 @@ module Paddle
         obj
       end
     end
+
+    def update(**params)
+      method_missing :update unless klass.respond_to? :update
+
+      primary_attributes = klass.method(:update).parameters.select { |(type, name)| type == :keyreq }.map(&:last) # Identified by whatever is a required named parameter.
+
+      primary_attributes.each do |attrib|
+        # ? Should we need to handle blank strings here?
+        params[attrib] = self[attrib] || self["#{attrib}_id"] # Handling for customer (customer_id) and id.
+      end
+
+      klass.public_send(:update, **params).each_pair { |key, val| self[key] = val } # Updating self
+
+      self
+    end
+
+    private
+
+    def klass
+      self.class
+    end
   end
 end
